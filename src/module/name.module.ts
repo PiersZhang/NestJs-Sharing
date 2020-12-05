05d85da9e99e4968803f657041709c6959d7a678
piers <Piers.Zhang@gaia.cn>
2020-12-05 13:23:51 +0800

    fix

--- src/module/name.module.ts
+++ src/module/name.module.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Piers.Zhang
  * @Date: 2020-12-04 16:42:57
- * @LastEditTime: 2020-12-05 09:58:40
+ * @LastEditTime: 2020-12-05 13:23:17
  * @LastEditors: Do not edit
  */
-import { Module, Global, HttpModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { NameController } from '../controller/name.controller';
 import { NameService } from '../service/name.service';
 
