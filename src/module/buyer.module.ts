33102a6134cc419983e14dd4e5100e069b3ce737
piers <Piers.Zhang@gaia.cn>
2020-12-18 22:40:37 +0800

    修改例子

--- src/module/buyer.module.ts
+++ src/module/buyer.module.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Piers.Zhang
  * @Date: 2020-12-04 16:20:42
- * @LastEditTime: 2020-12-15 15:50:39
+ * @LastEditTime: 2020-12-18 22:39:58
  * @LastEditors: Do not edit
  */
 import { Module, Global } from '@nestjs/common';
@@ -9,7 +9,7 @@ import { BuyerController } from 'src/controller/buyer.controller';
 import { BodyService } from '../service/body.service';
 
 @Module({
-    // providers: [BodyService],
+    // import: [BodyModule]
     controllers: [BuyerController],
 })
 export class BuyerModule { }
